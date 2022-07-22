e2eb40f68928e859bd47db3f55ee07878020bb0d
avhhust <avgystunpolek@gmail.com>
2022-07-22 13:36:02 +0300

    added configuration for Amazon stack and method to get S3 bucket

---
+++ src/main/java/com/pawinc/myblog/config/AmazonConfig.java
@@ -0,0 +1,35 @@
+package com.pawinc.myblog.config;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:tokens.properties")
+public class AmazonConfig {
+
+    @Value("${aws.access}")
+    private String accessKey;
+
+    @Value("${aws.secret}")
+    private String secretKey;
+
+    private AWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
+
+    @Bean
+    public AmazonS3 s3(){
+
+        return AmazonS3ClientBuilder
+                .standard()
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .withRegion(Regions.US_EAST_2)
+                .build();
+    }
+}
