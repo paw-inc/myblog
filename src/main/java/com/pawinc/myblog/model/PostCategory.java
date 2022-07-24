1d5ddbbf47998c463922a95407f70d19293f021c
avhhust <avgystunpolek@gmail.com>
2022-07-24 13:50:49 +0300

    updated PostCategory class

--- src/main/java/com/pawinc/myblog/model/PostCategory.java
+++ src/main/java/com/pawinc/myblog/model/PostCategory.java
@@ -2,6 +2,7 @@ package com.pawinc.myblog.model;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import java.util.Objects;
 
 @Entity
 @Table(name = "post_categories")
@@ -17,4 +18,45 @@ public class PostCategory {
     @Column(name = "parent_category")
     private Integer parentCategory;
 
+    public PostCategory() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public Integer getParentCategory() {
+        return parentCategory;
+    }
+
+    public void setParentCategory(Integer parentCategory) {
+        this.parentCategory = parentCategory;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PostCategory that = (PostCategory) o;
+        return Objects.equals(id, that.id) &&
+                Objects.equals(categoryName, that.categoryName) &&
+                Objects.equals(parentCategory, that.parentCategory);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, categoryName, parentCategory);
+    }
 }
